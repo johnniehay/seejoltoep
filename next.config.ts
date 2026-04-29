6b192c0efaf4e8f1a0f8459d5221c10df80451ee
johnniehay <johnnie.hay@gmail.com>
2026-04-29 21:12:29 +0200

    Make NEXT_PUBLIC_SERVER_URL env lookup dynamic

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
   experimental: {},
   images: {
     remotePatterns: [
-      ...[process.env.NEXT_PUBLIC_SERVER_URL].map((item) => {
+      ...[process.env['NEXT_PUBLIC_SERVER_URL']].map((item) => {
         const url = new URL(item)
         return {
           hostname: url.hostname,
--- src/utilities/getURL.ts
+++ src/utilities/getURL.ts
@@ -2,7 +2,7 @@ import canUseDOM from './canUseDOM'
 
 export const getServerSideURL = () => {
   return (
-    process.env.NEXT_PUBLIC_SERVER_URL ||
+    process.env['NEXT_PUBLIC_SERVER_URL'] ||
     (process.env.VERCEL_PROJECT_PRODUCTION_URL
       ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
       : 'http://localhost:3000')
