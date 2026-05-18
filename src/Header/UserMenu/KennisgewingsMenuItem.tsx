66274bccf709ac49279ddabb59d6512031dda498
johnniehay <johnnie.hay@gmail.com>
2026-05-18 23:20:11 +0200

    Make Kennisgewing Bell red and bouncing when notifications not enabled and show number of unacknowldged in icon

--- src/Header/UserMenu/KennisgewingsMenuItem.tsx
+++ src/Header/UserMenu/KennisgewingsMenuItem.tsx
@@ -9,10 +9,16 @@ import { Kennisgewing } from "@/payload-types";
 export function KennisgewingsMenuItem({ notificationSettingsSlot }: { notificationSettingsSlot: React.ReactNode }) {
   const { kennisgewinglogs, refreshKennisgewinglogs} = useKennisgewinglogs()
   const {subscription} = useContext(subscribeContext)
+  const unack_count = kennisgewinglogs?.filter((kennisgewinglog) => !kennisgewinglog.acknowledged).length ?? 0
   return (
     <Dialog>
       <DialogTrigger>
-        {subscription ? <IconBell/> : <IconBellOff/>}
+        <div className={"relative"}>
+          {subscription ? <IconBell/> : <IconBellOff color={"red"} className={"animate-bounce an"}/>}
+          {unack_count > 0 && <span className="absolute bottom-0 right-0 flex items-center justify-center w-3 h-3 text-[0.5rem] font-bold text-white bg-red-500 border-1 border-white rounded-full">
+            {unack_count}
+          </span>}
+        </div>
       </DialogTrigger>
       <DialogContent>
         <section id="kennisgewings" className="w-full max-w-4xl p-6 rounded-xl shadow-md">
@@ -22,7 +28,7 @@ export function KennisgewingsMenuItem({ notificationSettingsSlot }: { notificati
           </div>
           <div className="flex flex-col gap-2">
             {kennisgewinglogs ? kennisgewinglogs.filter(logentry => typeof logentry.kennisgewing !== 'string').map((logentry, index) => (
-              <KennisgewingCard key={index} notice={logentry.kennisgewing as Kennisgewing} noticeLog={logentry} />
+              <KennisgewingCard key={index} notice={logentry.kennisgewing as Kennisgewing} />
             )): (<p>Kennisgewings kon nie afgelaai word nie</p>)}
           </div>
         </section>
